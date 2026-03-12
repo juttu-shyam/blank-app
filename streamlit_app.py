c73810ecb836b55aa33129301addd262bf556684
Shyam Sundar <juttushyam@gmail.com>
2026-03-12 11:06:32 +0530

    Comment out ChatOllama code in streamlit_app.py
    Comment out code related to ChatOllama integration.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+# from langchain_ollama import ChatOllama
+# llm= ChatOllama(model='llama3.2')
+# history=[{'role':'system','content':'- you are a coding teacher - answer each question in two lines'}]
+# while True:
+#     question=input()
+#     history.append({'role':'user','content':question})
+#     responce=llm.invoke(history).content
+#     history.append({'role':'assistant','content':responce})
+#     print(responce)
